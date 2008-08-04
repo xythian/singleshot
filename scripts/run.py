01c86ef820847ea93d8559d724191d8723a55149
fox <fox@7e82af7f-0629-0410-be36-c8f2cdeb922f>
2008-08-04 04:14:19 +0000

    Setting ignore, adding run script.
    git-svn-id: svn+ssh://vault.xythian.com/singleshot/trunk@269 7e82af7f-0629-0410-be36-c8f2cdeb922f

---
+++ scripts/run.py
@@ -0,0 +1,13 @@
+#
+# Switch to wsgiref for the bare bones running.
+#
+import singleshot.serve
+import singleshot.ssconfig
+import os
+
+root = '../../sites/photos.xythian.com'
+root = os.path.abspath(root)
+templates = os.path.join(root, 'templates')
+
+store = singleshot.ssconfig.create_store(root, template_root=templates, baseurl='/')
+singleshot.serve.serve_http(store, addr='', port=8080)
